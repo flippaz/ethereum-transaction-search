6d95679e47097fdf92fe483841b428bb46b6d0a4
Frederick Rizardo <frederick.rizardo@beteasy.com.au>
2020-01-25 16:16:09 +1100

    initial commit

---
+++ EthereumTransactionSearch/Controllers/TransactionController.cs
@@ -0,0 +1,61 @@
+﻿using EthereumTransactionSearch.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EthereumTransactionSearch.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TransactionController : ControllerBase
+    {
+        [HttpGet("{blockNumber}")]
+        public async Task<IEnumerable<EthereumTransaction>> GetTransactions(string blockNumber, [FromQuery]string address)
+        {
+            var ethDetail = new List<EthereumTransaction>
+            {
+                new EthereumTransaction
+                {
+                    BlockHash = "blockHash1",
+                    BlockNumber = "blockNumber1",
+                    From = "address1",
+                    Gas = "gas1",
+                    GasPrice = "gasPrice1",
+                    Hash = "hash1",
+                    TransactionIndex = "index1",
+                    Value = "value1"
+                },
+                new EthereumTransaction
+                {
+                    BlockHash = "blockHash2",
+                    BlockNumber = "blockNumber2",
+                    From = "address2",
+                    Gas = "gas2",
+                    GasPrice = "gasPrice2",
+                    Hash = "hash2",
+                    TransactionIndex = "index2",
+                    Value = "value2"
+                },
+                new EthereumTransaction
+                {
+                    BlockHash = "blockHash3",
+                    BlockNumber = "blockNumber3",
+                    From = "address1",
+                    Gas = "gas3",
+                    GasPrice = "gasPrice3",
+                    Hash = "hash3",
+                    TransactionIndex = "index3",
+                    Value = "value3"
+                }
+            };
+
+            if (address != null && !string.IsNullOrWhiteSpace(address))
+            {
+                return await Task.FromResult(ethDetail.Where(t => t.From == address));
+            }
+
+            return await Task.FromResult(ethDetail);
+        }
+    }
+}
---
+++ EthereumTransactionSearch/Models/EthereumDetail.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+
+namespace EthereumTransactionSearch.Models
+{
+    public class EthereumDetail
+    {
+        public EthereumResult Result { get; set; }
+    }
+
+    public class EthereumResult
+    {
+        public IEnumerable<EthereumTransaction> Transactions { get; set; }
+    }
+
+    public class EthereumTransaction
+    {
+        public string BlockHash { get; set; }
+
+        public string BlockNumber { get; set; }
+
+        public string From { get; set; }
+
+        public string Gas { get; set; }
+
+        public string GasPrice { get; set; }
+
+        public string Hash { get; set; }
+
+        public string TransactionIndex { get; set; }
+
+        public string Value { get; set; }
+    }
+}
