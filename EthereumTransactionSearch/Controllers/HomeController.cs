e194b0a8d56db3052bdeede486136a48c5d3beb1
Frederick Rizardo <frederick.rizardo@beteasy.com.au>
2020-01-25 22:12:54 +1100

    clean up

--- EthereumTransactionSearch/Controllers/HomeController.cs
+++ EthereumTransactionSearch/Controllers/HomeController.cs
@@ -1,11 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using EthereumTransactionSearch.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-using EthereumTransactionSearch.Models;
+using System.Diagnostics;
 
 namespace EthereumTransactionSearch.Controllers
 {
@@ -18,15 +14,15 @@ namespace EthereumTransactionSearch.Controllers
             _logger = logger;
         }
 
-        public IActionResult Index()
-        {
-            return View();
-        }
-
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public IActionResult Index()
+        {
+            return View();
         }
     }
 }
--- EthereumTransactionSearch/Controllers/TransactionController.cs
+++ EthereumTransactionSearch/Controllers/TransactionController.cs
@@ -1,5 +1,4 @@
-﻿using EthereumTransactionSearch.Exceptions;
-using EthereumTransactionSearch.Services;
+﻿using EthereumTransactionSearch.Services;
 using Microsoft.AspNetCore.Mvc;
 using System.Threading.Tasks;
 
@@ -20,50 +19,6 @@ namespace EthereumTransactionSearch.Controllers
         public async Task<IActionResult> GetTransactions(string blockNumber, [FromQuery]string address)
         {
             return Ok(await _transactionService.GetTransactions(blockNumber, address));
-
-            //var ethDetail = new List<EthereumTransaction>
-            //{
-            //    new EthereumTransaction
-            //    {
-            //        BlockHash = "blockHash1",
-            //        BlockNumber = "blockNumber1",
-            //        From = "address1",
-            //        Gas = "gas1",
-            //        GasPrice = "gasPrice1",
-            //        Hash = "hash1",
-            //        TransactionIndex = "index1",
-            //        Value = "value1"
-            //    },
-            //    new EthereumTransaction
-            //    {
-            //        BlockHash = "blockHash2",
-            //        BlockNumber = "blockNumber2",
-            //        From = "address2",
-            //        Gas = "gas2",
-            //        GasPrice = "gasPrice2",
-            //        Hash = "hash2",
-            //        TransactionIndex = "index2",
-            //        Value = "value2"
-            //    },
-            //    new EthereumTransaction
-            //    {
-            //        BlockHash = "blockHash3",
-            //        BlockNumber = "blockNumber3",
-            //        From = "address1",
-            //        Gas = "gas3",
-            //        GasPrice = "gasPrice3",
-            //        Hash = "hash3",
-            //        TransactionIndex = "index3",
-            //        Value = "value3"
-            //    }
-            //};
-
-            //if (address != null && !string.IsNullOrWhiteSpace(address))
-            //{
-            //    return await Task.FromResult(Ok(ethDetail.Where(t => t.From == address)));
-            //}
-
-            //return await Task.FromResult(Ok(ethDetail));
         }
     }
 }
